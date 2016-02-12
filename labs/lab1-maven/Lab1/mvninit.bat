df746d3c2a24e0e3834ab5165afd4665c9a5caf4
L00131070 <L00131070@student.lyit.ie>
2016-02-12 22:19:59 +0000

    Final

--- labs/lab1-maven/Lab1/mvninit.bat
+++ labs/lab1-maven/Lab1/mvninit.bat
@@ -1,3 +1,4 @@
 set PATH=%PATH%;"X:\CEA\rootproject\labs\lab1-maven\Lab1\apache-maven-3.3.9\bin"
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_73
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_65
+
 
